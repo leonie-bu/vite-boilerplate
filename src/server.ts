a827f6f7a878daeb58a3ceff75612d82087538a4
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-16 12:32:25 +0200

    ✨ Load port from .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send('Hello');
 });
 

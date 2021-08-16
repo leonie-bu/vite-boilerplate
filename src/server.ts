c01b86c6dabf84635d6d467e25ea41bbbcb9efac
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-16 12:20:08 +0200

    ✨ Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Sunshine!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log('Server listenon port ${porty}!');
+});
